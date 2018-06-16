64ea534e9b4fc0af036882410c40c9ac9bec430c
Robert Fono <fonorobert@gmail.com>
2018-06-16 21:10:17 +0200

    rename toggl script, move api key to ~/.togglrc

---
+++ toggltest.py
@@ -0,0 +1,17 @@
+from TogglPy import Toggl
+import configparser, os 
+
+configParser = configparser.RawConfigParser()
+configFilePath = os.path.expanduser("~") + "/.togglrc"
+configParser.read(configFilePath)
+
+api_key = configParser.get('auth', 'api_token')
+
+toggl = Toggl()
+
+# this is a test accounts key. yeah, i know, api key in git is bad, whatevs
+toggl.setAPIKey(api_key)
+
+currententry = toggl.currentRunningTimeEntry()
+
+print(currententry)
