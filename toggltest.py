3b31031713956fe5da5c4c0fe5748334b87de92b
Robert Fono <fonorobert@gmail.com>
2018-06-17 12:39:54 +0200

    update config file paths, closes #2

--- toggltest.py
+++ toggltest.py
@@ -2,15 +2,17 @@ from TogglPy import Toggl
 import configparser, os 
 
 configParser = configparser.RawConfigParser()
-configFilePath = os.path.expanduser("~") + "/.togglkeypadrc"
+
+
+configFilePath = os.path.join(os.path.abspath(os.path.dirname(__file__))) + '/config.cfg'
 configParser.read(configFilePath)
 
-api_key = configParser.get('auth', 'api_token')
+api_token = configParser.get('auth', 'api_token')
 
 toggl = Toggl()
 
 # this is a test accounts key. yeah, i know, api key in git is bad, whatevs
-toggl.setAPIKey(api_key)
+toggl.setAPIKey(api_token)
 
 currententry = toggl.currentRunningTimeEntry()
 
