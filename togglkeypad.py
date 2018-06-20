ac67cc78304620cbc27a84635b7df58f449ed245
Robert Fono <fonorobert@gmail.com>
2018-06-20 18:43:46 +0200

    first pass at reorganizing config into dict, doesn't work yet

--- togglkeypad.py
+++ togglkeypad.py
@@ -11,8 +11,35 @@ config['api_token'] = configParser.get('toggl', 'api_token')
 config['ws_id'] = configParser.get('toggl', 'ws_id')
 config['device_path'] = configParser.get('device', 'input_device')
 config['keys'] = dict(configParser.items('keys'))
-config['projects'] = dict(configParser.items('projects'))
+timers = dict(configParser.items('timers'))
+print(type(timers))
+print(timers)
 
+
+print(type(timers['1']))
+print(timers['1'])
+print(timers['1'][0])
+timer1str = '{"projectid": 124015468, "description": "this is a TE"}'
+timer1 = dict(timer1str)
+print(type(timer1))
+print(timer1)
+
+exit()
+# turn timers into dicts
+config['timers'] = {}
+
+for key in timers:
+    print(timers[key])
+    config['timers'][key] = {}
+    timer_temp = dict(timers[key])
+    for attr in timer_temp:
+        config['timers'][key][attr] = timer_temp[attr]
+    print(config['timers'][key])
+    del timer_temp
+
+
+print(config['timers'])
+exit()
 
 # connect to Toggl
 toggl = TogglPy.Toggl()
